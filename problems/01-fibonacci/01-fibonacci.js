18bcaa368a0ac5c085e72634f6082c67bd019d32
Jeffrey Chang <you@example.com>
2019-11-17 10:30:53 -0500

    no1

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,21 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+       
+            if (n<=0){              
+                console.log("please input a number greater than 0");
+            }else if(n===1){  
+                    
+                    return 0
+               }else if(n===2){
+                   
+                   return 1
+               }else{
+                   
+                   return fib(n-1)+fib(n-2)
 
-}
+               }
+
+
+};
+console.log(fib(10))
